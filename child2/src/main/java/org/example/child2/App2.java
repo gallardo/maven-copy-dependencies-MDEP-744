383ecb46443b08141dff73b3e71e1cf26c37139f
gallardo <argrico@gmail.com>
2021-04-20 13:26:49 +0200

    Initial commit

---
+++ child2/src/main/java/org/example/child2/App2.java
@@ -0,0 +1,15 @@
+package org.example.child2;
+
+import org.example.child1.App1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2
+{
+    public static void main( String[] args )
+    {
+        App1.main(args);
+    }
+}
